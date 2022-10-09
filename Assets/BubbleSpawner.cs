34a80c81adb6466fcf6e7cb410eb6a46c3119a67
hktrollgames <hktrollgames@gmail.com>
2022-10-09 15:09:49 +0900

    버블 물리로 날리기

--- Assets/BubbleSpawner.cs
+++ Assets/BubbleSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,8 @@ public class BubbleSpawner : MonoBehaviour
     public float speed = 100;
     float rotateAngle = 0;
     public float maxAngle = 70;
+    public MovingBubble movingBubble;
+    public float force = 10;
     void Update()
     {
         if (Input.GetKey(KeyCode.D))
@@ -16,5 +19,17 @@ public class BubbleSpawner : MonoBehaviour
             rotateAngle = Mathf.Min(maxAngle, rotateAngle + speed * Time.deltaTime);
 
         fireDirTr.rotation = Quaternion.Euler(0, 0, rotateAngle);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            movingBubble.transform.position = fireDirTr.position;
+
+            var angle = -fireDirTr.rotation.eulerAngles.z;
+            var direction = new Vector2(
+                Mathf.Sin(Mathf.Deg2Rad * angle),  // x 좌표
+                Mathf.Cos(Mathf.Deg2Rad * angle)); // y 좌표 구하기
+
+            movingBubble.Fire(direction, force);
+        }
     }
 }
---
+++ Assets/MovingBubble.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBubble : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    internal void Fire(Vector2 direction, float force)
+    {
+        rb.AddForce(direction * force, ForceMode2D.Impulse);
+    }
+}
