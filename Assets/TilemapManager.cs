e2df7b3a08a49b26d4d35409396b26cbc513b88f
hktrollgames <hktrollgames@gmail.com>
2022-10-09 17:50:28 +0900

    매치된거 파괴 로직 작성중

--- Assets/TilemapManager.cs
+++ Assets/TilemapManager.cs
@@ -34,9 +34,53 @@ public class TilemapManager : MonoBehaviour
 
     internal void DestroyMatch3(Bubble bubble)
     {
-        //bubble.coord2D
-        //bubble.type
         //bubble타입정보, 위치 사용해서 3개이상 연결된 블럭들을 파괴하자.
+        CheckNearSameBubble(bubble.type, bubble.coord2D);
+        if(sameBubbleList.Count >= 3)
+        {
 
+        }
+
+        sameBubbleList.Clear();
+    }
+
+    private void CheckNearSameBubble(Bubble.Type type, Vector2Int coord2D)
+    {
+        AddSameBubble(type, coord2D, 0, 1);// 위쪽
+        AddSameBubble(type, coord2D, 0,-1);// 아래
+        AddSameBubble(type, coord2D,-1, 0);// 왼쪽
+        AddSameBubble(type, coord2D, 1, 0);// 오른족
+        //y가 짝수 일때 (-1,1)/(-1, -1)
+        if ( coord2D.y %2 == 0)
+        { 
+            AddSameBubble(type, coord2D,-1, 1);// 위쪽
+            AddSameBubble(type, coord2D, 0, 1);// 위쪽
+        }
+        else //y가 홀수 일때 (1,1)/(1, -1)
+        {
+            AddSameBubble(type, coord2D, 1, 1);// 위쪽
+            AddSameBubble(type, coord2D, 1,-1);// 위쪽
+        }
+    }
+
+    HashSet<Vector2Int> sameBubbleList = new();
+    HashSet<Vector2Int> alreadyCheck = new();
+    // 같은 타입일때 리스트에 더하자.
+    private void AddSameBubble(Bubble.Type type, Vector2Int coord2D, int x, int y)
+    {
+        var checkPos = new Vector2Int(x, y) + coord2D;
+        if (alreadyCheck.Contains(checkPos))
+            return; // 이미 체크 했다
+        alreadyCheck.Add(checkPos); 
+        
+        if (coordMap.ContainsKey(checkPos) == false)
+            return; // 보드에 버블이 없는 좌표다
+
+        var checkBubble = coordMap[checkPos];
+        if (checkBubble.type == type)
+        {
+            sameBubbleList.Add(checkPos);
+            CheckNearSameBubble(type, checkPos);
+        }
     }
 }
