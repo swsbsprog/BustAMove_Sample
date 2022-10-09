2b0725a00b624622bab3ac00e91b6c66a89d1cb8
hktrollgames <hktrollgames@gmail.com>
2022-10-09 14:42:31 +0900

    회전

---
+++ Assets/BubbleSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    public Transform fireDirTr;
+    public float speed = 100;
+    float rotateAngle = 0;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            rotateAngle += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            rotateAngle += -speed * Time.deltaTime;
+
+        fireDirTr.rotation = Quaternion.Euler(0, 0, rotateAngle);
+    }
+}
