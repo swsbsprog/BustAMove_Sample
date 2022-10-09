0d3fd950d36b3a5dc7839c482fab11cbd4b0061d
hktrollgames <hktrollgames@gmail.com>
2022-10-09 14:49:20 +0900

    화살 이동시 제한 각도 적용

--- Assets/BubbleSpawner.cs
+++ Assets/BubbleSpawner.cs
@@ -7,12 +7,13 @@ public class BubbleSpawner : MonoBehaviour
     public Transform fireDirTr;
     public float speed = 100;
     float rotateAngle = 0;
+    public float maxAngle = 70;
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-            rotateAngle += speed * Time.deltaTime;
         if (Input.GetKey(KeyCode.D))
-            rotateAngle += -speed * Time.deltaTime;
+            rotateAngle = Mathf.Max(-maxAngle, rotateAngle - speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            rotateAngle = Mathf.Min(maxAngle, rotateAngle + speed * Time.deltaTime);
 
         fireDirTr.rotation = Quaternion.Euler(0, 0, rotateAngle);
     }
